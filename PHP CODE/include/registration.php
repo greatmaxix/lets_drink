bec1918bb21fd4b99c2e4c91e046fa8cab375f42
Mxolisi Silabela <silabelamxolisi@gmail.com>
2019-12-09 22:21:14 +0100

    php folder - registration page added
    Signed-off-by: Mxolisi Silabela <silabelamxolisi@gmail.com>

---
+++ PHP CODE/include/registration.php
@@ -0,0 +1,70 @@
+<?php 
+
+include("include/functions.php");
+
+$pageTitle = "Register";
+$section = "register";
+$username = "";
+$password = "";
+$password_conf = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    session_start();
+    $username = clear_input($_POST["username"]);
+    $password = clear_input($_POST["password"]);
+    $password_conf = clear_input($_POST["password_conf"]);
+    register();
+
+    if (array_key_exists("success", $_SESSION)){
+        if ($_SESSION['success']){
+            redirect("index.php");
+        }    
+    }
+}
+ 
+?>
+<html>
+<head>
+	<title><?php echo $pageTitle; ?></title>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+
+	<div class="header">
+
+		<div class="wrapper">
+
+			<h1 class="branding-title">Personal Media Library</h1>
+
+		</div>
+
+	</div>
+		
+<div id="content">
+
+<div class="section page">
+    <div class="wrapper">
+        <h1>Be a part of our community</h1>
+        
+        <form method="post" action="register.php">
+            <table>
+            <tr>
+                <th><label for="username">Username</label></th>
+                <td><input type="text" name="username" value="<?php echo $username; ?>"></td>
+            </tr>
+            <tr>
+                <th><label for="password">Password</label></th>
+                <td><input type="password" name="password" value="<?php echo $password; ?>"></td>
+            </tr>
+            <tr>
+                <th><label for="password">Confirm your assword</label></th>
+                <td><input type="password" name="password_conf" value="<?php echo $password; ?>"></td>
+            </tr>
+
+            </table>
+            <input type="submit" value="Register" name = "submit"/>
+        </form>
+    </div>
+</div>
+
+<?php include("include/footer.php"); ?>
